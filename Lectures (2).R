18c01790eac0a7aa57e2da17933b69d95c58f104
eastbourne1904 <alasdair.hazen@gmail.com>
2016-01-27 18:11:29 -0600

    Practical Machine Learning - Project

--- Lectures (2).R
+++
@@ -1,6 +0,0 @@
-library(kernlab);
-data(spam);
-set.seed(333)
-smallSpam <- spam[sample(dim(spam)[1],size=10),]
-spamLabel <- (smallSpam$type=="spam")*1 + 1
-plot(smallSpam$capitalAve,col=spamLabel)
